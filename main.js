3b77afa5f55171912658613ad828d1c848c51563
ReomaKitazoe <riazyu.ktz@gmail.com>
2012-08-01 18:11:52 +0900

    gassyuku 8/1
    + sheep
    + zIndex

--- main.js
+++ main.js
@@ -4,96 +4,99 @@ window.onload = function(){
 
     var game = new Game(320, 320);
 
-    game.fps = 30;
+    game.fps = 50;
     
-    game.preload("chara4.gif","bg.png","icon1.png");
+    game.preload("chara4.gif","bg.gif","icon1.png","sheep5.png");
     
     //初期設定
     game.life = 3;
     touching = false;
     game.time = 0;
-    
     items = [];
     
     
     game.onload = function(){
         
         //マップを設定
-        var map = new Sprite(320, 640);
-        map.image = game.assets["bg.png"];
+        var map = new Sprite(320, 1280);
+        map.image = game.assets["bg.gif"];
         map.y = -map.height + game.height;
-		    game.rootScene.addChild(map);
+		    map._element.style.zIndex = 1;
+        game.rootScene.addChild(map);
 		    
 		    //自機の設定
 		    var car = new Sprite(32, 32);
 		    car.image = game.assets["chara4.gif"];
-		    car.x = 160;
-		    car.y = game.height - car.height;
+		    car.x = 160 - 16;
+		    car.y = 320 - 50;
+        car._element.style.zIndex = 3;
 		    game.rootScene.addChild(car);
 		    
-		    //フレームイベントが発生したら処理
+		    
+       //フレームイベントが発生したら処理
 		    car.addEventListener(Event.ENTER_FRAME, function(){
 		        if (game.input.left && (car.x > 0)){ car.x = car.x - 4;	 }
 			      if (game.input.right && (car.x < game.width-car.width)){ car.x = car.x + 4; }
-			      if (game.input.up && (car.y > 80)){ car.y = car.y - 4;	 }
-			      if (game.input.down && (car.y < game.height-car.height)){ car.y = car.y + 4; }
 		    });
 		    
 		    //障害物の追加
 		    game.addObstacle = function(x, speed) {
 		    
 		        //障害物の生成
-		        var obstacle = new Sprite(16, 16);
-		        obstacle.image = game.assets["icon1.png"];
+		        var obstacle = new Sprite(31, 27);
+		        obstacle.image = game.assets["sheep5.png"];
 		        obstacle.x = x;
-		        obstacle.y = -16;
+		        obstacle.y = -50;
 		        obstacle.frame = 15;
 		        obstacle.speed = speed;
+
+            
 		        game.rootScene.addChild(obstacle);
-		    
+		        
 		        //スプライトの定期処理
 		        obstacle.addEventListener(Event.ENTER_FRAME, function() {
 		            obstacle.y += obstacle.speed;
-		            //自機と衝突
-		            if (car.within(obstacle, 16)) {
+		            obstacle._element.style.zIndex = obstacle.y + obstacle.height;
+                //自機と衝突
+		            if (car.within(obstacle, 20 )) {
 		                game.life -= 1;
-		                if (game.life <= 0) {
-		                    game.end(game.score, "あなたのスコアは" + game.score);
-		                }
-		                game.rootScene.removeChild(obstacle);
-
-		            
+                    game.rootScene.removeChild(obstacle);
 		            }
-		            
+                else if (obstacle.y > 640) {
+		                game.rootScene.removeChild(obstacle);
+		            }
+		            else if (game.life === 0) {
+                game.end(game.score, "あなたのスコアは" + game.score);
+		            }
 		        });
         };
 		    
 		    //シーンの定期処理
-		    game.tick = 32 * 10;
-		    game.rootScene.addEventListener(Event.ENTER_FRAME, function() {
-		        game.tick--;
-		        if (game.tick > 0) {
+        game.rootScene.addEventListener(Event.ENTER_FRAME, function() {
+		        game.time++;
+            if (game.fps > 0) {
 		            //障害物の生成
-		            if ((game.tick % 10) === 0) {
+		            if ((game.time % 10) === 0) {
 		                var x     = Math.floor(Math.random() * 300);
 		                var speed = 4;
 		                game.addObstacle(x,speed);
 		            }
+            }
 		        
-		        }
-		    });
-		    
+        });
+
 		    //スコアを表示
 		    //draw.text.jsプラグインを使用
 		    scoreLabel = new MutableText(8, 8, game.width, "");
         scoreLabel.score = 0;
-        
+        scoreLabel._element.style.zIndex = 5;
         
         //ライフを表示
         lifeLabel = new MutableText(320 - 135, 8, game.width, "");
         lifeLabel.addEventListener('enterframe', function(){
             this.text = "LIFE " + "OOOOOOOOO".substring(0, game.life);
         });
+        lifeLabel._element.style.zIndex = 4;
         game.rootScene.addChild(lifeLabel);
 
         
@@ -117,6 +120,5 @@ window.onload = function(){
 	}
 	
 	game.start();	// ゲーム処理開始
-
 
 }
